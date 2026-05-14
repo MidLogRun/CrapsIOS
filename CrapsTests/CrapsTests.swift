86651a08589f7e5e004eec55f7b0a32826845b7a
matthewlong <mattbasslong@gmail.com>
2026-05-14 00:05:28 -0600

    Initial Commit

---
+++ CrapsTests/CrapsTests.swift
@@ -0,0 +1,19 @@
+//
+//  CrapsTests.swift
+//  CrapsTests
+//
+//  Created by Matthew Long on 5/14/26.
+//
+
+import Testing
+@testable import Craps
+
+struct CrapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
